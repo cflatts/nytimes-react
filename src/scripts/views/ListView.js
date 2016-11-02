52734a958943addfe7b1ee21b6b6745905edf404
Caitlin Flattery <caitlinflattery@gmail.com>
2016-11-02 12:11:30 -0500

    passed props all the way down to single article

--- src/scripts/views/ListView.js
+++ src/scripts/views/ListView.js
@@ -3,7 +3,7 @@ import Header from './header'
 
 var ListView = React.createClass({
     render: function() {
-        console.log('list view component', this.props.listColl.models)
+        // console.log('list view component', this.props.listColl.models)
         return(
             <div className = 'listView'>
                 <Header />
@@ -16,15 +16,29 @@ var ListView = React.createClass({
 var ArticleContainer = React.createClass({
 
     _makeArticles: function() {
+        var jsxArray = []
+        var propsArr = this.props.articleColl.models
 
+        propsArr.forEach(function(articleModel) {
+            jsxArray.push(<Article articleModel = {articleModel} key = {articleModel.cid}/>)
+        })
+        return jsxArray
     },
 
     render: function() {
-        console.log('article container component', this.props.articleColl)
+        // console.log('article container component', this.props.articleColl)
         return(
             <div className = 'articleContainer'>
                 {this._makeArticles()}
             </div>
+        )
+    }
+})
+
+var Article = React.createClass({
+    render: function() {
+        return(
+            <div></div>
         )
     }
 })
